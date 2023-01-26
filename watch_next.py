d293d3f892cf8904c485ba164ca0a6459d0271e7
Attila Levai <atilevai@gmail.com>
2023-01-26 10:46:37 +0000

    Comments update

--- watch_next.py
+++ watch_next.py
@@ -10,24 +10,32 @@ GREEN = "\033[92m"
 BLUE = "\033[94m"
 YELLOW = "\033[33m"
 
-#loading the language model and assign it to a variable.
-nlp = spacy.load( 'en_core_web_md' )
+#==========Functions==============
 
+#movie chosing function: 
 def watch_next(movie_watched,movie_watched_desc,movie_store):
+    #variables setting
     score = 0
     movie_to_watch = ""
     #passing the string through the language model
     movie_watched_desc_nlp = nlp(movie_watched_desc)
     print(f"{UNDERLINE}{BOLD}Similarity score:{RESET}")
+    #for loop to go through the movies in the dictionary, key = movie; value = desc
     for key, value in movie_store.items():
+        #similarity score check
         score_check = movie_watched_desc_nlp.similarity(nlp(value))
         print(f"{key} - {score_check}")
+        #using if to store the highest score and the movie name
         if score < score_check:
             score = score_check
             movie_to_watch = key
     print("-"*100)
     print(f"As you watched {BOLD}{BLUE}{movie_watched}{RESET}, you should chose next: {BOLD}{GREEN}{movie_to_watch}{RESET}\n\n{UNDERLINE}{BOLD}Description:{RESET} {movie_store[movie_to_watch]}")
 
+#==========Main Program=============
+
+#loading the language model and assign it to a variable.
+nlp = spacy.load( 'en_core_web_md' )
 
 #User watched Planet Hulk with the description below
 movie_watched = "Planet Hulk"
@@ -39,6 +47,7 @@ planet Sakaar where he is sold into slavery and trained as a gladiator.'''
 #passing the string through the language model
 movie_watched_nlp = nlp(movie_watched)
 
+#open the movie file
 file = open("movies.txt", "r",encoding="utf-8")
 
 #dictionary store for the movies
@@ -47,8 +56,9 @@ movie_store = {}
 #loading the movies to the dictionary
 for i in file:
     movie_store[i[:7]] = i[9:].strip("\n")
-
+    
+#close the movie file
 file.close()
-#print(movie_store)
 
+#calling the movie selecting function
 watch_next(movie_watched,movie_watched_desc,movie_store)
